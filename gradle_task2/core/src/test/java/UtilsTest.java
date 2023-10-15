6fd80765146d65ab7f9429b7a0a18bd12c3e27f8
sergey turpakov <paradise666hell@mail.ru>
2023-10-15 15:18:04 +0300

    test(core): add unit test for Utils.isAllPositiveNumbers method

---
+++ gradle_task2/core/src/test/java/UtilsTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilsTest {
+
+    private final Utils utils = new Utils();
+    @Test
+    public void testIsAllPositiveNumbers() {
+        assertTrue(utils.isAllPositiveNumbers("124", "1346"));
+        assertFalse(utils.isAllPositiveNumbers("6543", "-1456"));
+        assertFalse(utils.isAllPositiveNumbers("0", "666"));
+    }
+}
