9507422aefd5a234b52117398c1ecc95cffc50f1
sergey turpakov <paradise666hell@mail.ru>
2023-10-15 14:38:49 +0300

    feat: configure jar manifest

--- gradle_task1/build.gradle.kts
+++ gradle_task1/build.gradle.kts
@@ -10,3 +10,11 @@ java {
 repositories {
     mavenCentral()
 }
+
+tasks.jar {
+    manifest {
+        attributes["Implementation-Title"] = "Utils"
+        attributes["Implementation-Version"] = "1.3.5"
+    }
+    archiveFileName.set("utils-1.3.5.jar")
+}
